7e2c99a52807112c99967e37c45839ea6f855cdd
Sam Singer <samsingerjr@gmail.com>
2015-01-09 15:12:42 -0600

    edit app_mailer.rb to allow email to appended @auburn.edu

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -4,6 +4,7 @@ class AppMailer < ActionMailer::Base
  
   def welcome_email(user)
     @user = user
+    #@user.email = @user.email'@auburn.edu'
     mail(to: @user.email, subject: "COMM1000 & COMM1003")
   end
 end
